3de9d8553ca24cd3908abaae2ff1eb9cc2060b0d
Dominique Nguyen <domnguyen@gmail.com>
2019-01-10 19:02:00 -0800

    finish contact/personaltrainer page

---
+++ src/components/contacts/Contacts.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import ContactsComponent from './ContactsComponent'
+
+class Contacts extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            contacts: '',
+            name: '',
+            phone: '',
+            twittter: '',
+            instagram: '',
+            data: ''
+ 
+
+        }
+    }
+
+    componentDidMount() {
+        Accounts.getAllAccounts(this.getAllAccountsSuccess, this.getAllAccountsError)
+    }
+
+    getAllAccountsSuccess = resp => {
+        console.log(resp)
+        this.setState({ data:resp.data.item })
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className="layout-content">
+                    <h3 className="font-weight-bold py-3 mb-4">
+                        Personal Trainers
+                    </h3>
+
+                    <div class="row contacts-col-view">
+                    {this.state.data.match((par) => {
+                        return <ContactsComponent
+                            key = {par.id}
+                            id = {par.id}
+                            picture = {par.picture}
+                            name = {par.name}
+                            phone = {par.phone}
+                            email = {par.email}
+                            twitter = {par.twitter}
+                            facebook = {par.facebook}
+                            instagram = {par.instagram}
+                        />
+                    })}
+                    </div>
+                </div>
+            </React.Fragment>
+
+        )
+    }
+
+    
+
+
+}
+
+export default Contacts
---
+++ src/components/contacts/ContactsComponent.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const ContactsComponent = props => {
+    
+    return(
+        <div key={props.id} id={props.id}>
+            <div className="contacts-col col-12">
+                <div className="card mb-4">
+                    <div className="card-body">
+                        <img src={props.picture} className="contact-content-img rounded-circle" alt="" />
+                        <div className="contact-content-about">
+                            <h4 className="contact-content-name mb-1">
+                                {props.name}
+                            </h4>
+                        </div>
+                        <div className='small'>
+                            {props.phone}
+                        </div>
+                        <hr className="border-light"></hr>
+                        <div>
+                          <a href={'//www.'+ props.email} className="text-secondary">
+                            <span className="ion ion-md-mail"></span>
+                          </a> &nbsp;&nbsp;
+                          <span className="text-lighter">|</span> &nbsp;&nbsp;
+                          <a href={'//www.'+ props.twitter} className="text-twitter">
+                            <span className="ion ion-logo-twitter"></span>
+                          </a> &nbsp;&nbsp;
+                          <a href={'//www.'+ props.facebook} className="text-facebook">
+                            <span className="ion ion-logo-facebook"></span>
+                          </a> &nbsp;&nbsp;
+                          <a href={'//www.'+ props.instagram} className="text-instagram">
+                            <span className="ion ion-logo-instagram"></span>
+                          </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ContactsComponent
