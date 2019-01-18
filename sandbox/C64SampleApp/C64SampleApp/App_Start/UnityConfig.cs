759f441e50ff15e3610444b3644c29e476fe9bb9
Dominique Nguyen <domnguyen@gmail.com>
2019-01-18 14:22:50 -0800

    moved folder to client

--- sandbox/C64SampleApp/C64SampleApp/App_Start/UnityConfig.cs
+++ sandbox/C64SampleApp/C64SampleApp/App_Start/UnityConfig.cs
@@ -1,50 +1,23 @@
 using C64AdoNetSample.Person;
-using System;
-using System.Web.Mvc;
+using System.Web.Http;
 using Unity;
-using Unity.AspNet.Mvc;
+using Unity.WebApi;
 
 namespace C64SampleApp
 {
     public static class UnityConfig
     {
-        #region Unity Container
-        private static Lazy<IUnityContainer> container =
-          new Lazy<IUnityContainer>(() =>
-          {
-              var container = new UnityContainer();
-              RegisterTypes(container);
-              return container;
-          });
-
-        /// <summary>
-        /// Configured Unity Container.
-        /// </summary>
-        public static IUnityContainer Container => container.Value;
-        #endregion
-
-        /// <summary>
-        /// Registers the type mappings with the Unity container.
-        /// </summary>
-        /// <param name="container">The unity container to configure.</param>
-        /// <remarks>
-        /// There is no need to register concrete types such as controllers or
-        /// API controllers (unless you want to change the defaults), as Unity
-        /// allows resolving a concrete type even if it was not previously
-        /// registered.
-        /// </remarks>
-        public static void RegisterTypes(IUnityContainer container)
+        public static void RegisterComponents()
         {
-            // NOTE: To load from web.config uncomment the line below.
-            // Make sure to add a Unity.Configuration to the using statements.
-            // container.LoadConfiguration();
+			var container = new UnityContainer();
 
-            // TODO: Register your type's mappings here.
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
 
+            // e.g. container.RegisterType<ITestService, TestService>();
             container.RegisterType<IPersonService, PersonService>();
 
-
-            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+            GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
         }
     }
 }
